a94fed1786995ae5c81253eb7d7c554011b9a451
xjarvik <54638086+xjarvik@users.noreply.github.com>
2021-01-04 17:43:45 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,1 @@
-#!/bin/bash
-
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash && source ~/.bashrc && nvm install 14 && wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb && sudo dpkg -i packages-microsoft-prod.deb && sudo apt-get update && sudo apt-get install -y apt-transport-https && sudo apt-get update && sudo apt-get install -y dotnet-sdk-3.1 && wget https://github.com/xjarvik/misc/raw/main/flatc -P flatbuffers && chmod u+x flatbuffers/flatc && echo "export PATH=$PATH:$(pwd)/flatbuffers" >> ~/.bashrc && source ~/.bashrc
